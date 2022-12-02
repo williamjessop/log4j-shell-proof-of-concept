795545e1db171ea993881873c94bb49b4cf98064
William Jessop <willjessop12@gmail.com>
2022-12-01 19:13:39 -0700

    figured out webgoat lesson submission

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
-sudo docker build . -t log4j-proof && sudo docker run -d --network host log4j-proof 
+mkdir /tmp/log4j-docker
+
+sudo docker build . -t log4j-proof && sudo docker run -d --network host --mount type=bind,source="/tmp/log4j-docker",target="/tmp" log4j-proof 
 pip install -r requirements.txt
 
 # webport is for http server that serves exploit
